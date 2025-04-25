45928384006d726eba96cd067635da5a60c27805
Fedor <pichuginf2016@mail.ru>
2025-04-25 12:36:59 +0500

    adds timer in test

--- tests/conveyor_test.py
+++ tests/conveyor_test.py
@@ -3,6 +3,8 @@ from compressor.bwt import bwt_encode, bwt_decode
 from compressor.mtf import mtf_encode, mtf_decode
 from compressor.zle import zle_encode, zle_decode
 from compressor.ari import arithmetic_encode, arithmetic_decode
+import time
+
 
 def test_full_pipeline():
     text = Path("tests/test.txt").read_text(encoding="utf-8")
@@ -21,14 +23,15 @@ def test_full_pipeline():
     zle_out = zle_encode(mtf_out, marker=zle_marker)
     encoded = arithmetic_encode(zle_out)
 
-    print(f'encoded: {encoded}')
+    # print(f'encoded: {encoded}')
 
     # Обратное преобразование
     decoded = arithmetic_decode(encoded, len(zle_out))
     mtf_decoded = mtf_decode(zle_decode(decoded, marker=zle_marker), alphabet)
     text_out = bwt_decode(mtf_decoded, bwt_index)
 
-    print(f'text_out: {text_out}')
+    # print(f'text_out: {text_out}')
 
     assert text == text_out, "Ошибка в пайплайне сжатия"
     print("\033[32mПайплайн сжатия прошел успешно\033[0m")
+    
--- tests/tests_pipeline.py
+++ tests/tests_pipeline.py
@@ -1,3 +1,4 @@
+import time
 from tests.conveyor_test import test_full_pipeline
 from tests.module_tests import (
     test_ari_empty, test_ari_large_data, test_ari_single_symbol, test_bwt_identity, test_bwt_mtf_pipeline, test_mtf_identity,
@@ -18,8 +19,11 @@ def run_module_tests():
     test_ari_single_symbol()
 
 def run_conveyor_test():
+    start_time = time.time()
     test_full_pipeline()
+    end_time = time.time()
+    print(f"\033[34mВремя выполнения: {end_time - start_time:.3f} сек\033[0m")
 
 if __name__ == '__main__':
     run_module_tests()
-    # run_conveyor_test()
+    run_conveyor_test()
